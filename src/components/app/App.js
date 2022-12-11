3d92c3cecfd4def95772563e64cbc2d7da816e8c
RamzinDenis <txforyou@rambler.ru>
2022-12-11 17:51:04 +0300

    Обновил зависимости проекта

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Grid from "../grid";
-import "antd/dist/antd.css";
 import styles from "./app.module.scss";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
 import RestartBtn from "../restart-btn";
 
 const App = () => {
-	return (
-		<Provider store={store}>
-			<h2 className={styles.title}>Color Flipper</h2>
-			<div className={styles.wrapper}>
-				<RestartBtn classN={styles.button} />
-				<Grid />
-			</div>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <h2 className={styles.title}>Color Flipper</h2>
+      <div className={styles.wrapper}>
+        <RestartBtn classN={styles.button} />
+        <Grid />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
